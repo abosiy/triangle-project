9a4cf183325b083f455fb129eaa2fadbc0299a25
abosiy1996 <abosiy1996@gmail.com>
2024-03-22 14:31:31 +0200

    [FEAT] - create triangle and method variation

---
+++ src/com/bosiy/MainApplication.java
@@ -0,0 +1,16 @@
+package com.bosiy;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+        Triangle[] array = new Triangle[3];
+
+        array[0] = new Triangle(12, 12, 12);
+        array[1] = new Triangle(12, 12, 10);
+        array[2] = new Triangle(8, 10, 7);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i].variation());
+        }
+    }
+}
---
+++ src/com/bosiy/Triangle.java
@@ -0,0 +1,28 @@
+package com.bosiy;
+public class Triangle {
+    private final int sideA;
+    private final int sideB;
+    private final int sideC;
+
+    public Triangle(int sideA, int sideB, int sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    public String treangle() {
+        return "side a = " + sideA + "; " + " side b = " + sideB + "; " +
+                " side c = " + sideC + "; ";
+    }
+    public String variation() {
+        if (sideA == sideB && sideB == sideC) {
+            System.out.println("Этот треугольник равносторонний.");
+        }
+        if (sideA == sideB && sideA != sideC) {
+            System.out.println("Этот треугольник равнобедренный.");
+        }
+        if (sideA != sideB && sideB != sideC && sideA != sideC) {
+            System.out.println("Этот треугольник разносторонний");
+        }
+        return treangle();
+    }
+}
